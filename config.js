2f740e002c650913914bf4ea3e56523b88250669
gebilaofan <gebilaofl@163.com>
2022-01-06 21:03:00 +0800

    feat: 定时自动签到抽奖测试

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // 配置文件
 module.exports = {
   // 需要手动填写
-  cookie: '_ga=GA1.2.704776625.1640487702; MONITOR_WEB_ID=c5d533be-fd08-4cbe-8095-a58c9cc4fad1; passport_csrf_token_default=0b65d43de96a7026639d1c12e5c7fd95; passport_csrf_token=0b65d43de96a7026639d1c12e5c7fd95; passport_auth_status=79213aa0d2b135ecb26f7e4a6c5b5a19,; passport_auth_status_ss=79213aa0d2b135ecb26f7e4a6c5b5a19,; sid_guard=ebe2b6bd3bbecaaf542a88061a406def|1640487730|5184000|Thu,+24-Feb-2022+03:02:10+GMT; uid_tt=f6ed78d297afcc5d70489842dfc07f77; uid_tt_ss=f6ed78d297afcc5d70489842dfc07f77; sid_tt=ebe2b6bd3bbecaaf542a88061a406def; sessionid=ebe2b6bd3bbecaaf542a88061a406def; sessionid_ss=ebe2b6bd3bbecaaf542a88061a406def; sid_ucp_v1=1.0.0-KDEwY2U1MDk0OTI4MWY2MDgzNzhhODJhMWQ3ZmM4OGI1YWNlODk3NTIKFwi3s_DA_fWVBBCytp-OBhiwFDgCQPEHGgJsZiIgZWJlMmI2YmQzYmJlY2FhZjU0MmE4ODA2MWE0MDZkZWY; ssid_ucp_v1=1.0.0-KDEwY2U1MDk0OTI4MWY2MDgzNzhhODJhMWQ3ZmM4OGI1YWNlODk3NTIKFwi3s_DA_fWVBBCytp-OBhiwFDgCQPEHGgJsZiIgZWJlMmI2YmQzYmJlY2FhZjU0MmE4ODA2MWE0MDZkZWY; n_mh=DI-bDWSdI2DCP4p4nGOcOxE4dlMg2yJnXXb9pGxWTSA; _gid=GA1.2.553389825.1641259470; _tea_utm_cache_2608={"utm_source":"gold_browser_extension"}',
+  cookie: '_ga=GA1.2.704776625.1640487702; MONITOR_WEB_ID=c5d533be-fd08-4cbe-8095-a58c9cc4fad1; passport_csrf_token_default=0b65d43de96a7026639d1c12e5c7fd95; passport_csrf_token=0b65d43de96a7026639d1c12e5c7fd95; n_mh=DI-bDWSdI2DCP4p4nGOcOxE4dlMg2yJnXXb9pGxWTSA; _gid=GA1.2.553389825.1641259470; _tea_utm_cache_2608={"utm_source":"gold_browser_extension"}; _tea_utm_cache_2018=undefined; passport_auth_status=de24c0346a3dd178872b44b367cf5612,79213aa0d2b135ecb26f7e4a6c5b5a19; passport_auth_status_ss=de24c0346a3dd178872b44b367cf5612,79213aa0d2b135ecb26f7e4a6c5b5a19; sid_guard=7111e31da85df7f6616c78cd0208fb4a|1641469323|5184000|Mon,+07-Mar-2022+11:42:03+GMT; uid_tt=0d5bac1981d238ff7052721a8e1acda9; uid_tt_ss=0d5bac1981d238ff7052721a8e1acda9; sid_tt=7111e31da85df7f6616c78cd0208fb4a; sessionid=7111e31da85df7f6616c78cd0208fb4a; sessionid_ss=7111e31da85df7f6616c78cd0208fb4a; sid_ucp_v1=1.0.0-KDRjZWUyM2RjZWI2MDFmMDFjYjJlM2M1ZDJkYjc3OWU2Y2Q0ZTg3NWYKFwi3s_DA_fWVBBCLq9uOBhiwFDgCQPEHGgJsZiIgNzExMWUzMWRhODVkZjdmNjYxNmM3OGNkMDIwOGZiNGE; ssid_ucp_v1=1.0.0-KDRjZWUyM2RjZWI2MDFmMDFjYjJlM2M1ZDJkYjc3OWU2Y2Q0ZTg3NWYKFwi3s_DA_fWVBBCLq9uOBhiwFDgCQPEHGgJsZiIgNzExMWUzMWRhODVkZjdmNjYxNmM3OGNkMDIwOGZiNGE',
   aid: '2608',
   uuid: '7045840987474527755',
   // 请求地址
@@ -14,8 +14,10 @@ module.exports = {
     getCheckStatus: 'https://api.juejin.cn/growth_api/v1/get_today_status',
     // 查询抽奖
     getlotteryStatus: '/growth_api/v1/lottery_config/get',
+    // 查寻当前矿石余额
     // 抽奖
-    lottery: '/growth_api/v1/lottery/draw',
+    draw: '/growth_api/v1/lottery/draw',
     // 沾喜气
+    dipLucky: '/growth_api/v1/lottery_lucky/dip_lucky'
   }
 }
--- index.js
+++ index.js
@@ -1,27 +1,98 @@
 const axios = require('axios')
 const config = require('./config')
+
 axios.defaults.baseURL = config.baseUrl
+axios.defaults.headers['cookie'] = config.cookie
+
+/**
+ * 查看今天是否已经签到
+ *
+ * @return {Boolean} 是否签到过 
+ */
+getCheckStatus = async () => {
+  try {
+    const { data } = await axios({
+      url: config.api.getCheckStatus,
+      method: 'get'
+    })
+    if (data.err_msg === 'success' && data.err_no === 0) {
+      return data.data
+    } else {
+      console.warn('❌ 查询签到失败！')
+      throw `getCheckStatus requies error【${data.err_msg}】`
+    }
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+/**
+ * 查询免费抽奖次数
+ *
+ * @return {Boolean} 是否有免费抽奖次数
+ */
+const getlotteryStatus = async () => {
+  try {
+    const { data } = await axios({ url: config.api.getlotteryStatus, method: 'get' })
+    if (data.err_msg === 'success' && data.err_no === 0) {
+      return data.data.free_count === 0
+    } else {
+      console.warn('❌ 查询免费抽奖失败！')
+      throw `getlotteryStatus requies error【${data.err_msg}】`
+    }
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+
+/**
+ * 抽奖 先查询是否有免费抽奖次数
+ *
+ */
+const draw = async () => {
+  const freeCount = await getlotteryStatus()
+  if (freeCount) {
+    // 没有免费抽奖次数
+    console.warn('今日免费抽奖以用完 🥲')
+    return
+  }
+
+  // 开始抽奖
+  const { data } = await axios({ url: config.api.draw, method: 'post' })
+  if (data.err_no === 0 && data.err_msg === 'success') {
+    console.log(`恭喜你抽到【${data.data.lottery_name}】🎉`)
+  } else {
+    console.error(`抽奖失败! 🙁【${data.err_msg}】`)
+  }
+  console.log(data, '----draw')
+}
+
+
 /**
  * 签到
  * 
  */
 const checkIn = async () => {
+  const checkRes = await getCheckStatus()
+
+  if (checkRes) {
+    console.log('今日已经签到 ✅')
+    // TODO: 发送邮件
+    return
+  }
+
+  // 签到
+  const { data } = await axios({ url: config.api.checkIn, method: 'post' })
+  console.log(data, '-----checkInRes')
+  if (data.err_no === 0 && data.err_msg === 'success') {
+    console.log('签到成功! 🎉')
+    // 签到成功 去抽奖
+    draw()
+  } else {
+    // 签到失败
+    console.log(`签到失败 😔:: 【${data.err_msg}】`)
+  }
 }
 
-const getCheckStatus = async () => {
-  const aid = config.aid
-  const uuid = config.uuid
-  const {data} = await axios({
-    url: config.api.getCheckStatus,
-    method: 'get',
-    data: {
-      aid,
-      uuid
-    },
-    headers: {
-      Coolie: config.cookie
-    },
-  })
-  console.log(data, '---checkStatus')
-}
-getCheckStatus()
+// checkIn()
